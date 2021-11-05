604f3a8f0d65567223f2e542ad3b7671a15c65e1
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2021-11-05 21:27:52 +0300

    fix: fix solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 
 async function copyFiles() {
+  await  fs.promises.rm(path.resolve(__dirname, 'files-copy'),{ recursive: true, force:true });
   await  fs.promises.mkdir(path.resolve(__dirname, 'files-copy'),{ recursive: true });
   await fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
     files.forEach(item => {
