aae62604937724a1a3caa246459118a83bcf01b4
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2021-11-05 21:18:28 +0300

    add: add solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+
+async function copyFiles() {
+  await  fs.promises.mkdir(path.resolve(__dirname, 'files-copy'),{ recursive: true });
+  await fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+    files.forEach(item => {
+      fs.copyFile(path.resolve(__dirname, 'files', item), path.resolve(__dirname, 'files-copy', item), (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+}
+
+copyFiles();
