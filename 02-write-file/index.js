c79848c35dcc830d09f96bd4d0c714c3053050bd
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2021-11-08 23:03:55 +0300

    fix: fix 02 solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,16 +6,16 @@ const {stdout, stdin} = process;
 
 
 
-fs.writeFile(path.join(__dirname, 'text.txt'), '', err => {if(err) throw err});
+fs.writeFile(path.join(__dirname, 'text.txt'), '', err => {if(err) throw err;});
 
 
 stdout.write('Hello\n');
 stdin.on('data', data=>{
   let str = data.toString().trim();
   if (str === 'exit' || str === 'SIGINT' ){
-
-    process.exit();
     stdout.write('good bye!');
+    process.exit();
+
   } else {
     fs.appendFile(
       path.join(__dirname, 'text.txt'),
